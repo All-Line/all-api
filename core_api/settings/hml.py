70ef69e27ba570c18be82dc11c2b0f9d68bca4a6
how-dev <howard.medeiros@gmail.com>
2023-04-18 20:37:03 -0300

    devops: Up system to homolog environment

--- core_api/settings/hml.py
+++ core_api/settings/hml.py
@@ -1,6 +1,5 @@
-from decouple import config as env
-
 from .base import *  # noqa
+from storages.backends.s3boto3 import S3Boto3Storage
 
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
@@ -13,13 +12,13 @@ DEV_EMAIL = "squirre.lair.startnow@gmail.com"
 
 WHITENOISE_STATIC_PREFIX = "/static/"
 
-YOUR_S3_BUCKET = "startnow-zappa-static"
-AWS_STORAGE_BUCKET_NAME = YOUR_S3_BUCKET
+S3_BUCKET = "allline-zappa-static"
+AWS_STORAGE_BUCKET_NAME = S3_BUCKET
 
 STATICFILES_STORAGE = "django_s3_storage.storage.StaticS3Storage"
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-AWS_S3_BUCKET_NAME_STATIC = YOUR_S3_BUCKET
+AWS_S3_BUCKET_NAME_STATIC = S3_BUCKET
 
-AWS_S3_CUSTOM_DOMAIN = f"{YOUR_S3_BUCKET}.s3.amazonaws.com"
+AWS_S3_CUSTOM_DOMAIN = f"{S3_BUCKET}.s3.amazonaws.com"
 STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 MEDIA_URL = AWS_S3_CUSTOM_DOMAIN
