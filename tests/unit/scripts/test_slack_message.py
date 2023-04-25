8c14aa388045f7e7b8e40f996b6b9ea413805c4c
how-dev <howard.medeiros@gmail.com>
2023-04-25 20:47:28 -0300

    Add slack message integration on application deploy

---
+++ tests/unit/scripts/test_slack_message.py
@@ -0,0 +1,23 @@
+from unittest.mock import patch
+
+from scripts.slack_message import send_message_to_slack
+
+
+@patch("scripts.slack_message.argparse")
+@patch("scripts.slack_message.requests")
+@patch("scripts.slack_message.env")
+def test_send_message_to_slack(mock_env, mock_requests, mock_argparse):
+    send_message_to_slack()
+
+    mock_argparse.ArgumentParser.assert_called_once()
+
+    parser = mock_argparse.ArgumentParser.return_value
+
+    parser.add_argument.assert_called_once_with("message", type=str)
+    parser.parse_args.assert_called_once()
+
+    args = parser.parse_args.return_value
+
+    mock_requests.post.assert_called_once_with(
+        mock_env.return_value, json={"text": args.message}
+    )
